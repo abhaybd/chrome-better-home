e3274e1bebfa58dbceb002ebf4da267ce75785d8
Abhay D <a.deshpande012@gmail.com>
2022-01-02 22:17:46 -0800

    Fixed favicon fetcher; removed dependency on cloud func

--- homepage/src/FaviconAPI.js
+++ homepage/src/FaviconAPI.js
@@ -2,23 +2,26 @@ import {storageDelete, storageGet, storageKeys, storageSet} from "./Storage";
 
 const CACHE_LIFESPAN = 1000 * 60 * 60 * 24 * 7; // 1 week in milliseconds
 const FAVICON_PREFIX = "FAVICON_";
-const API_PREFIX = "https://us-central1-chrome-better-home.cloudfunctions.net/getIcon?domain=";
+const API_PREFIX = "https://www.google.com/s2/favicons?domain=";
 
 export async function getFaviconData(host) {
     let newExpiryDate = Date.now() + CACHE_LIFESPAN;
     let cacheName = FAVICON_PREFIX + host;
     let cached = storageGet(cacheName);
-    if (cached) {
+    // only use cached item if not expired
+    if (cached && cached.expiry > Date.now()) {
         return cached.data;
     }
     let response = await fetch(API_PREFIX + host);
-    let data = await response.text();
+    let bufRaw = await response.text();
     if (response.ok) {
         // cache the data
+        let b64 = Buffer.from(bufRaw).toString("base64");
+        let data = "data:" + response.headers["content-type"] + ";base64," + b64;
         storageSet(cacheName, {expiry: newExpiryDate, data: data});
         return data;
     } else {
-        console.error(`An error occurred while fetching favicon data! Status=${response.status}, Message=${data}`)
+        console.error(`An error occurred while fetching favicon data for host=${host}! Status=${response.status}, Message=${bufRaw}`);
     }
 }
 
